8b76529392e9b58e555fbd586217a821b706550f
MarcosVel <marcosvelosodev@gmail.com>
2024-08-07 22:33:55 -0300

    chore: add google fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
+import {
+  Roboto_400Regular,
+  Roboto_700Bold,
+  useFonts,
+} from "@expo-google-fonts/roboto";
+import { StatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import { Loading } from "./src/components";
 import { SignIn } from "./src/screens";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" backgroundColor="transparent" translucent />
       <SignIn />
     </ThemeProvider>
   );
