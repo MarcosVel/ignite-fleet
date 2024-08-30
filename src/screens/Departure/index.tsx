1a488ab37a835b5fb6e4550b20bf2a95f5121725
MarcosVel <marcosvelosodev@gmail.com>
2024-08-30 16:24:23 -0300

    feat: sckrollView and keyboardAvoidingView on departure

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,12 @@
 import React, { useRef } from "react";
-import { Keyboard, TextInput, TouchableWithoutFeedback } from "react-native";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TextInput,
+  TouchableWithoutFeedback,
+} from "react-native";
 import {
   Button,
   Header,
@@ -8,30 +15,45 @@ import {
 } from "../../components";
 import { Container, Content } from "./styles";
 
+const keyboardAvoidingBehavior =
+  Platform.OS === "android" ? "height" : "position";
+
 export default function Departure() {
   const descriptionRef = useRef<TextInput>(null);
+
+  function handleDeparture() {
+    console.log("ok");
+  }
 
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <Container>
         <Header title="Saída" />
 
-        <Content>
-          <LicensePlateInput
-            label="Placa do veículo"
-            placeholder="QLX2360"
-            onSubmitEditing={() => descriptionRef.current?.focus()} // move to next input
-            returnKeyType="next"
-          />
+        <KeyboardAvoidingView
+          style={{ flex: 1 }}
+          behavior={keyboardAvoidingBehavior}
+          enabled
+        >
+          <ScrollView>
+            <Content>
+              <LicensePlateInput
+                label="Placa do veículo"
+                placeholder="QLX2360"
+                onSubmitEditing={() => descriptionRef.current?.focus()} // move to next input
+                returnKeyType="next"
+              />
 
-          <TextAreaInput
-            ref={descriptionRef}
-            label="Finalidade"
-            placeholder="Vou utilizar o veículo para..."
-          />
+              <TextAreaInput
+                ref={descriptionRef}
+                label="Finalidade"
+                placeholder="Vou utilizar o veículo para..."
+              />
 
-          <Button title="Registrar saída" />
-        </Content>
+              <Button title="Registrar saída" onPress={handleDeparture} />
+            </Content>
+          </ScrollView>
+        </KeyboardAvoidingView>
       </Container>
     </TouchableWithoutFeedback>
   );
