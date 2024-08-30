5f4c87b4b89930e4241663624a914f30dc48ece8
MarcosVel <marcosvelosodev@gmail.com>
2024-08-30 17:38:14 -0300

    feat: validating license plate

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,5 +1,6 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import {
+  Alert,
   Keyboard,
   KeyboardAvoidingView,
   Platform,
@@ -14,15 +15,26 @@ import {
   TextAreaInput,
 } from "../../components";
 import { Container, Content } from "./styles";
+import { licensePlateValidate } from "../../utils/licensePlateValidate";
 
 const keyboardAvoidingBehavior =
   Platform.OS === "android" ? "height" : "position";
 
 export default function Departure() {
+  const licensePlateRef = useRef<TextInput>(null);
   const descriptionRef = useRef<TextInput>(null);
+  const [data, setData] = useState({
+    licensePlate: "",
+    description: "",
+  });
 
   function handleDeparture() {
-    console.log("ok");
+    if (licensePlateValidate(data.licensePlate)) {
+      return;
+    }
+
+    licensePlateRef.current?.focus();
+    Alert.alert("Placa inválida", "Por favor, informe a placa correta.");
   }
 
   return (
@@ -38,16 +50,29 @@ export default function Departure() {
           <ScrollView>
             <Content>
               <LicensePlateInput
+                ref={licensePlateRef}
                 label="Placa do veículo"
                 placeholder="QLX2360"
                 onSubmitEditing={() => descriptionRef.current?.focus()} // move to next input
                 returnKeyType="next"
+                onChangeText={(text) =>
+                  setData((prevData) => ({
+                    ...prevData,
+                    licensePlate: text,
+                  }))
+                }
               />
 
               <TextAreaInput
                 ref={descriptionRef}
                 label="Finalidade"
                 placeholder="Vou utilizar o veículo para..."
+                onChangeText={(text) =>
+                  setData((prevData) => ({
+                    ...prevData,
+                    description: text,
+                  }))
+                }
               />
 
               <Button title="Registrar saída" onPress={handleDeparture} />
