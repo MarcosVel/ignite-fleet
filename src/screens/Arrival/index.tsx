c2f5da05233d052cbf82afa250f0cf5b4c3a2856
MarcosVel <marcosvelosodev@gmail.com>
2024-09-01 00:47:05 -0300

    feat: styles for arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,6 +1,14 @@
 import { useRoute } from "@react-navigation/native";
 import React from "react";
-import { Container, Title } from "./styles";
+import { Button, Header } from "../../components";
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from "./styles";
 
 type RouteParamsProps = {
   id: string;
@@ -14,7 +22,24 @@ export default function Arrival() {
 
   return (
     <Container>
-      <Title>index</Title>
+      <Header title="Chegada" />
+
+      <Content>
+        <Label>Placa do veículo</Label>
+        <LicensePlate>XXX0000</LicensePlate>
+
+        <Label>Finalidade</Label>
+        <Description>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit
+          perferendis sapiente officiis odio dolorum placeat, distinctio
+          facilis. Iusto nemo, voluptatibus beatae temporibus, quis, neque
+          tenetur hic numquam aut magni iure!
+        </Description>
+
+        <Footer>
+          <Button title="Registrar Chegada" />
+        </Footer>
+      </Content>
     </Container>
   );
 }
