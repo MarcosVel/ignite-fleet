3d6621f124794ec927ad0be0317149076eb462bb
MarcosVel <marcosvelosodev@gmail.com>
2024-09-05 02:02:19 -0300

    feat: background task

---
+++ src/tasks/backgroundLocTask.ts
@@ -0,0 +1,36 @@
+import { Accuracy, startLocationUpdatesAsync } from "expo-location";
+import * as TaskManager from "expo-task-manager";
+
+export const BACKGROUND_TASK_NAME = "location-tracking";
+
+TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+  try {
+    if (error) {
+      throw error;
+    }
+
+    const { coords, timestamp } = data.locations[0];
+
+    const currentLocation = {
+      latitude: coords.latitude,
+      longitude: coords.longitude,
+      timestamp,
+    };
+
+    console.log("currentLocation", currentLocation);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+export async function startLocationTask() {
+  try {
+    await startLocationUpdatesAsync(BACKGROUND_TASK_NAME, {
+      accuracy: Accuracy.Highest,
+      distanceInterval: 1,
+      timeInterval: 1000,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
